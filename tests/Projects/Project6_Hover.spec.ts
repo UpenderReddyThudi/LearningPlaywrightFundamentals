babcbbf4941789db05a17dad9e9e58beb8b710a0
UpenderReddyThudi <upender7@gmail.com>
2026-05-10 13:44:13 +1000

    Push latest changes

---
+++ tests/Projects/Project6_Hover.spec.ts
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test';
+
+test('Assignment6 - Hover', async({page})=>{
+
+    
+    await page.goto('https://app.thetestingacademy.com/playwright/widgets/hover-menu');
+    await page.getByTestId('nav-add-ons').hover();
+    await page.getByTestId('test-id-Meal').click();
+
+    let JSONMessage = await page.getByTestId('hover-output').innerText();
+    console.log(JSONMessage);
+    expect(JSONMessage).toContain('"testId": "test-id-Meal"');
+
+    await page.waitForTimeout(5000);
+
+});
